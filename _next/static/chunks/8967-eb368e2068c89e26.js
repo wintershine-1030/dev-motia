72a9f7abd3f4503e96a87c52293b0d36921ded52
wintershine-1030 <wintershine1030@gmail.com>
2025-08-25 05:51:37 +0300

    up

--- _next/static/chunks/8967-eb368e2068c89e26.js
+++ _next/static/chunks/8967-eb368e2068c89e26.js
@@ -87,7 +87,7 @@
           , n = {
             deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
             imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-            path: "/_next/image",
+            path: "/_next/static/media",
             loader: "default",
             loaderFile: "",
             domains: [],
@@ -446,7 +446,7 @@
           , g = {
             deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
             imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-            path: "/_next/image",
+            path: "/_next/static/media",
             loader: "default",
             dangerouslyAllowSVG: !1,
             unoptimized: !1
