bd13d624f4a37aff40dd168f61e0d7ecfaa36861
wintershine-1030 <wintershine1030@gmail.com>
2025-09-03 10:08:57 +0300

    up

--- _next/static/chunks/8193-31d84b7d795cf6a0.js
+++ _next/static/chunks/8193-31d84b7d795cf6a0.js
@@ -10,9 +10,11 @@
         nE: () => r,
         vp: () => n,
         yN: () => s,
+        mail: () => m,
       });
       let r = "https://github.com/ObiyaDev/Obiya",
         l = "https://x.com/Obiyadev",
+        m = "mailto:hello@obiya.dev",
         n = "https://github.com/ObiyaDev/Obiya-examples/tree/main/examples";
     },
     25938: (e, t, i) => {
@@ -534,6 +536,26 @@
                                     "text-white/60 transition-colors ease-in-out hover:text-white",
                                   "aria-label": "Follow us on X (Twitter)",
                                   children: x.Xn,
+                                }),
+                                (0, s.jsx)(h(), {
+                                  href: p.mail,
+                                  target: "_blank",
+                                  className:
+                                    "text-white/60 transition-colors ease-in-out hover:text-white",
+                                  "aria-label": "Contact us via email",
+                                  children: (0, s.jsx)("svg", {
+                                    width: "20",
+                                    height: "16",
+                                    viewBox: "0 0 20 16",
+                                    fill: "none",
+                                    xmlns: "http://www.w3.org/2000/svg",
+                                    children: [
+                                      (0, s.jsx)("path", {
+                                        d: "M18 0H2C0.9 0 0.01 0.9 0.01 2L0 14C0 15.1 0.9 16 2 16H18C19.1 16 20 15.1 20 14V2C20 0.9 19.1 0 18 0ZM18 4L10 9L2 4V2L10 7L18 2V4Z",
+                                        fill: "currentColor"
+                                      })
+                                    ]
+                                  })
                                 }),
                               ],
                             }),
